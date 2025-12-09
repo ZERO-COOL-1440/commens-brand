c3edc58efaa6be67090f997ea1367d7cb0add96d
Zero <zerocool@gmail.com>
2025-12-09 22:32:16 +0330

    fetch added

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 /*-----------------------------------*\
   #script.js
 \*-----------------------------------*/
+// -- Global --
+const timer = 2000;
+const MAX_CHARS = 150;
 
 const textareaEL = document.querySelector(".form__textarea");
 const counterEL = document.querySelector(".counter");
 const formEL = document.querySelector(".form");
 const feddsEL = document.querySelector(".feedbacks");
 const submitEL = document.querySelector(".submit-btn");
-const timer = 2000;
 
 const inputHandler = () => {
-  const maxChars = 150;
-
   const CharsTyped = textareaEL.value.length;
 
-  const charsLeft = maxChars - CharsTyped;
+  const charsLeft = MAX_CHARS - CharsTyped;
 
   // number of charaters left
   counterEL.textContent = charsLeft;
@@ -22,17 +22,16 @@ const inputHandler = () => {
 
 textareaEL.addEventListener("input", inputHandler);
 
-//submit component
+//form component
+
 const submitHandler = (event) => {
   event.preventDefault();
   const text = textareaEL.value;
 
   if (text.includes("#") && text.length >= 5) {
-    formEL.classList.add("form--valid");
-    changeClass("form--valid");
+    showVisualIndicator("valid");
   } else {
-    formEL.classList.add("form--invalid");
-    changeClass("form--invalid");
+    showVisualIndicator("invalid");
     textareaEL.focus();
     return;
   }
@@ -61,17 +60,46 @@ const submitHandler = (event) => {
         <p class="feedback__date">${dayAgo === 0 ? "NEW" : `${dayAgo}d`}</p>
     </li>
 `;
-
   feddsEL.insertAdjacentHTML("beforeend", feedItem);
   textareaEL.value = "";
   submitEL.blur();
-  counterEL.textContent = "150";
+  counterEL.textContent = MAX_CHARS;
 };
 
-function changeClass(className) {
+const showVisualIndicator = (textCheck) => {
+  const className = textCheck === "valid" ? "form--valid" : "form--invalid";
+  formEL.classList.add(className);
   setTimeout(() => {
     formEL.classList.remove(className);
   }, timer);
-}
+};
 
 formEL.addEventListener("submit", submitHandler);
+
+fetch("https://bytegrad.com/course-assets/js/1/api/feedbacks")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.feedbacks.forEach((feedItem) => {
+      const feedsItem = `
+    <li class="feedback">
+        <button class="upvote">
+            <i class="fa-solid fa-caret-up upvote__icon"></i>
+            <span class="upvote__count">${feedItem.upvoteCount}</span>
+        </button>
+        <section class="feedback__badge">
+            <p class="feedback__letter">${feedItem.badgeLetter}</p>
+        </section>
+        <div class="feedback__content">
+            <p class="feedback__company">${feedItem.company}</p>
+            <p class="feedback__text">${feedItem.text}</p>
+        </div>
+        <p class="feedback__date">${
+          feedItem.daysAgo === 0 ? "NEW" : `${feedItem.daysAgo}d`
+        }</p>
+    </li>
+`;
+      feddsEL.insertAdjacentHTML("beforeend", feedsItem);
+    });
+  });
